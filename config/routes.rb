e071a799f87c174a5258269d227fafb8c088a5a7
Archit <archit.mishra@1mg.com>
2021-06-26 10:13:29 +0530

    changed home page and added its test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static/home'
-  get 'static/help'
+  get 'static/home'       # these static has nothing to do with static keyword
+  get 'static/help'       # they are just named so by us bcoz' they are static pages.
   get 'static/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  # root 'application#hello'
+  root 'static#home'
 end
--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_home_url
     assert_response :success
