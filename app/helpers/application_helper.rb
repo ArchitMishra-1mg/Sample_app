e1f0158b810b5a479790e938ac7cbc82c99a4266
Archit <archit.mishra@1mg.com>
2021-06-26 12:22:46 +0530

    made helper for title creation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # First custom application helper method
+
+  def full_page_title(title = '')
+    base_title = "RoR Tutorials"
+    if title.empty?
+      base_title
+    else
+      title + " | " + base_title
+    end
+  end
 end
