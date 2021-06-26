060bd4c5ff16de0dbf6e64fb38eb53c5a74a8ce8
Archit <archit.mishra@1mg.com>
2021-06-26 12:06:30 +0530

    added dynamic title for static pages

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -10,15 +10,18 @@ class StaticControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_home_url
     assert_response :success
+    assert_select "title", "Home | RoR Tutorials"
   end
 
   test "should get help" do
     get static_help_url
     assert_response :success
+    assert_select "title", "Help | RoR Tutorials"
   end
 
   test "should get about" do
     get static_about_url
     assert_response :success
+    assert_select "title", "About | RoR Tutorials"
   end
 end
