89bd336221c547952113d27aa2b95e2318576b28
Archit <archit.mishra@1mg.com>
2021-06-27 11:51:33 +0530

    added Users controller and rel func and attached to signup button

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
-  # these static has nothing to do with static keyword
-  # # they are just named so by us bcoz' they are static pages.
+  ## get 'users/new'
+  ## these static has nothing to do with static keyword
+  ## they are just named so by us bcoz' they are static pages.
 
-  # get 'static/home'
-  # get 'static/help'
-  # get 'static/about'
+  ## get 'static/home'
+  ## get 'static/help'
+  ## get 'static/about'
   #
-  # changin the above methods to newer and better ones.
+  ## changin the above methods to newer and better ones.
 
   get '/help', to: 'static#help'
   get '/about', to: 'static#about'
-  # get '/home', to: 'static#home'
+  get '/signup', to: 'users#new'
+
+  ## get '/home', to: 'static#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'application#hello'
   root 'static#home'
